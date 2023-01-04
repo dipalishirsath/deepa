77662b29c2ae8353189869d8da1df683b627e493
HarshadBorse <121923219+HarshadBorse@users.noreply.github.com>
2023-01-04 14:51:33 +0530

    Updated
    your code is upto date

--- GIT_GIT_KY_HE/src/Demo.java
+++ GIT_GIT_KY_HE/src/Demo.java
@@ -8,11 +8,21 @@ public class Demo {
 	{
 		System.out.println("hey eclipse,can i integrate with u");
 	}
+	public void m3()
+	{
+		System.out.println("My answer is noooooooo");
+	}
+	public void m4()
+	{
+		System.out.println("Tula jamnar nai rahu de");
+	}
 	public static void main(String[] args)
 	{
 		Demo d = new Demo();
 		d.m1();
 		d.m2();
+		d.m3();
+		d.m4();
 		System.out.println("hey git");
 	}
 }
